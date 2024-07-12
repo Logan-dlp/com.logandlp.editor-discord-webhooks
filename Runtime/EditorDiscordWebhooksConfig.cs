934a60d6b8ca9eebd4322315e0eb0d15f7561ff0
Logan <pix.59700@gmail.com>
2024-07-12 17:29:02 +0200

    bug to change value

--- Runtime/EditorDiscordWebhooksConfig.cs
+++ Runtime/EditorDiscordWebhooksConfig.cs
@@ -1,8 +1,12 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class EditorDiscordWebhooksConfig : ScriptableObject
 {
+    [WriteOnly]
     public string Name;
+    [WriteOnly]
     public string WebhooksAPI;
+    [WriteOnly]
     public bool Logging = true;
 }
